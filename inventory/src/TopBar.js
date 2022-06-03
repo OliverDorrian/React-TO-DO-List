24c8b4d6648931638b34fde4cf170324cf8c7469
Oliver Dorrian <dorrianolfc@gmail.com>
2022-06-03 15:29:30 +0100

    Changes To-Do List

---
+++ inventory/src/TopBar.js
@@ -0,0 +1,14 @@
+function TopBar(){
+    return(        
+    <div id="top-bar"> 
+        <div id="left-top-bar">
+            <p>To-DO List 📝,</p>
+        </div>
+        <div id="right-top-bar">
+            <p>Log-Out</p>
+        </div> 
+    </div>
+    );
+}
+
+export default TopBar;
