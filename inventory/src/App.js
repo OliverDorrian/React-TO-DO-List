fc9b1100dc94ac07f9db2205daa074073e52313d
Oliver Dorrian <dorrianolfc@gmail.com>
2022-06-04 14:58:06 +0100

    list storing

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <CreateBackGround/>
         <TopBar/>
         <MainContent/>
+        
     </div>
   );
 }
--- inventory/src/main.js
+++ inventory/src/main.js
@@ -1,34 +1,54 @@
-
+import React from 'react';
 
 function MainContent(){
     return (
         <div id="main-content">
             <Buttons/>
-
         </div>
     );
 }
 
+class Buttons extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value: '',
+            numbers: []
+            };
+    }
 
-function Buttons(){
-    return (
-        <div id="row-buttons">          
-            <div id="row-buttons-left">
-                <input type="text" class="search-box" id="search-box"></input>
+    updateInputValue(evt){
+        this.setState({
+          value: evt.target.value,
+          numbers: this.state.numbers.concat([evt.target.value])
+        });
+    }
+
+    loadList(){
+        this.state.numbers.push(this.state.value);
+        
+    }
+
+    render () {
+
+        return(
+            <div>
+                <div id="row-buttons">          
+                    <div id="row-buttons-left">
+                        <input type="text" onChange={evt => this.updateInputValue(evt)} value={this.state.value} class="search-box" id="search-box"></input>
+                    </div>
+                    <div id="row-buttons-right">
+                        <button type="click" onClick={this.loadList()} class="button" id="task-create">Add Item</button>
+                    </div>
+                </div>
+                <div id="list-of-items">
+                    {this.state.listItems}
+                </div>                 
             </div>
-            <div id="row-buttons-right">
-                <button type="click" class="button" id="task-create">Add Item</button>
-            </div>
-        </div>
-    );
-}
-
-function List(passedName){
-    return (
-        <div>
-
-        </div>
-    );
+     
+              
+        )        
+    }
 }
 
 
